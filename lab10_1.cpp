aba5434a7e37fbf702c66e831c60b1942a55dd1f
wayu <iarahtuyaw@gmail.com>
2022-12-27 15:37:38 +0700

    1finished

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+	double loan,rate,pay,dok;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin>> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin>>pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +24,24 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << fixed << setprecision(2); 	
+	int i=1;
+	while(loan!=0){
+		cout << setw(13) << left << i;
+		cout << setw(13) << left << loan;
+		dok=loan*(rate/100);
+		cout << setw(13) << left << dok;
+		loan=loan+dok;
+		cout << setw(13) << left << loan;
+		if(loan<pay){
+			pay=loan;
+		}
+		cout << setw(13) << left << pay;
+		loan=loan-pay;
+		cout << setw(13) << left << loan;
+		cout << "\n";
+		i++; 
+	}
 	
 	return 0;
 }
